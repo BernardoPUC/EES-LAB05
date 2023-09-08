79ff02114d4c9e8ed2667ca74044e1964d8814fc
Bernardo Aquino <bernardoaquinoc@gmail.com>
2023-09-08 11:40:03 -0300

    code

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+// 1 - Comment
+// 2 - Comment
+public class Main {
+
+
+    public static void main(String[] args)                                              {
+
+
+
+        // 3 - Comment
+        // 4 - Comment
+        System.out.printf("Engenharia Econômica para Software | Bernardo Aquino Capello Coelho\n");
+
+        // 5 - Comment
+        for (int i = 1; i <= 5; i++) {
+
+            // 6 - Comment
+            // 7 - Comment
+            System.out.println("i = " + i);
+
+    // 8 - Comment
+                        }
+    }
+    // 9 - Comment
+    // 10 - Comment
+}
